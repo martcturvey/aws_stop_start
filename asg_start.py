bc326b8fd9074ed9f180163407be7464e2e79760
martin <martincturvey@gmail.com>
2019-03-07 16:42:00 +0000

    Adding status checks before resuming ASG

--- asg_start.py
+++ asg_start.py
@@ -16,6 +16,13 @@ parser.add_argument("asg_name_tag", type=str,
 		    help="The Name tag of the target ASG")
 args = parser.parse_args()
 
+ec2_connect = boto.ec2.connect_to_region(aws_region)
+reservations = ec2_connect.get_all_instances(filters={'instance-state-name' : 'stopped', 'tag:Name' : args.asg_name_tag})
+
+if len(reservations) == 0:
+  print(args.asg_name_tag + ": no EC2 matches in a stopped state")
+  sys.exit(2)
+
 asg = boto3.client('autoscaling')
 paginator = asg.get_paginator('describe_auto_scaling_groups')
 asg_list = []
@@ -51,23 +58,39 @@ ec2 = boto3.resource('ec2')
 
 print("EC2s:")
 
+started_list = []
+
 for ec2_instance in ec2_list:
   instance = ec2.Instance(ec2_instance)
   ec2_start = instance.start()
   print(ec2_start['StartingInstances'][0]['InstanceId'] + ' is ' + ec2_start['StartingInstances'][0]['CurrentState']['Name'])
+  started_list.append(ec2_start['StartingInstances'][0]['InstanceId'])
 
-ec2_up = False
-sleep_loop = 10
-ec2_connect = boto.ec2.connect_to_region(aws_region)
-all_ec2 = len(ec2.connect.get_all_instances(filters={'tag:Name':args.asg_name_tag+'*'}))
+# Allow metadata to be populated
+time.sleep(10)
 
-while (not ec2_up) and (sleep_loop < 10):
-  time.sleep(30)
-  running_ec2 = len(ec2.connect.get_all_instances(filters={'instance-state-name' : 'running', 'tag:Name':args.asg_name_tag+'*'}))
-  if all_ec2 == running_ec2:
-    ec2_up = True
+all_healthy = False
+sleep_loop = 0
+
+while (not all_healthy) and (sleep_loop < 10):
+  sleep_loop += 1
+  instance_healthy = 0
+  for status in ec2.meta.client.describe_instance_status(InstanceIds=started_list)['InstanceStatuses']:
+    if (status['SystemStatus']['Status'] == 'ok') and (status['InstanceStatus']['Status'] == 'ok'):
+      print(status['InstanceId'] + " is passing status checks")
+      instance_healthy += 1
+    else:
+      print(status['InstanceId'] + " is not passing status checks")
+  if (instance_healthy < len(started_list)):
+    print("Waiting for all instances to pass status checks")
+    time.sleep(30)
   else:
-    sleep_loop += 1
+    print("All instances passing status checks, resuming ASG scaling processes")
+    all_healthy = True
+
+if (not all_healthy) and (sleep_loop == 10):
+  print("Some instances failed to pass status checks within time limit")
+  sys.exit(2)
 
 asg_resume = asg.resume_processes(
   AutoScalingGroupName=asg_list[0]['AutoScalingGroupName'],
